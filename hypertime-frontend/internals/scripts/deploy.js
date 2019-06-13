7d38dccddb8df2d230951a62930fbd44ffed9b6a
Oscar Ljungberg <oscar.ljungberg@annevo.se>
2019-06-13 22:35:46 +0200

    wip

--- hypertime-frontend/internals/scripts/deploy.js
+++ hypertime-frontend/internals/scripts/deploy.js
@@ -1,5 +1,5 @@
 const shelljs = require('shelljs');
 
 shelljs.exec(
-  'az storage blob upload-batch -s /var/lib/jenkins/workspace/Hypertime_jenkinsfile/hypertime-frontend/build --destination=$web --connection-string=DefaultEndpointsProtocol=https;AccountName=hypertime;AccountKey=/hKquqMB4jLoma30s9HdVXJhiCIf++3loLZQe2nz0RPjPXiMs7xd8B96vFGsvr8lPVpMMVyp7rbBXBQ9+R+XYA==;EndpointSuffix=core.windows.net',
+  'az storage blob upload-batch -s /var/lib/jenkins/workspace/Hypertime_jenkinsfile/hypertime-frontend/build --destination=$web --connection-string DefaultEndpointsProtocol=https;AccountName=hypertime;AccountKey=/hKquqMB4jLoma30s9HdVXJhiCIf++3loLZQe2nz0RPjPXiMs7xd8B96vFGsvr8lPVpMMVyp7rbBXBQ9+R+XYA==;EndpointSuffix=core.windows.net',
 );
